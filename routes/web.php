ab3bbf782825acd4c6af94654f2348a494a51abe
raimeiga <yusuke612@hotmail.com>
2024-01-18 13:57:23 +0900

    20.1 カートの中身を表示

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ReviewController;
 
 /*
@@ -18,6 +19,10 @@ use App\Http\Controllers\ReviewController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(CartController::class)->group(function () {
+    Route::get('users/carts', 'index')->name('carts.index');
 });
 
 Route::controller(UserController::class)->group(function () {
