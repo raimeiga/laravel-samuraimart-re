d0ee1f1dc5fe778e4aa29cfac949b2c8bfc68b23
raimeiga <yusuke612@hotmail.com>
2024-01-18 13:27:11 +0900

    16.3 コントローラーを作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,52 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function show(User $user)
-    {
-        //
-    }
+    public function mypage()
+     {
+         $user = Auth::user();
+ 
+         return view('users.mypage', compact('user'));
+     }
 
     /**
      * Show the form for editing the specified resource.
@@ -58,7 +24,9 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        $user = Auth::user();
+ 
+        return view('users.edit', compact('user'));
     }
 
     /**
@@ -70,17 +38,15 @@ class UserController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(User $user)
-    {
-        //
+        $user = Auth::user();
+ 
+         $user->name = $request->input('name') ? $request->input('name') : $user->name;
+         $user->email = $request->input('email') ? $request->input('email') : $user->email;
+         $user->postal_code = $request->input('postal_code') ? $request->input('postal_code') : $user->postal_code;
+         $user->address = $request->input('address') ? $request->input('address') : $user->address;
+         $user->phone = $request->input('phone') ? $request->input('phone') : $user->phone;
+         $user->update();
+ 
+         return to_route('mypage');
     }
 }
