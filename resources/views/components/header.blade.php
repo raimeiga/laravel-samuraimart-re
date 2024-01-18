87a14665d6c8c0fcdc44c78605d5c582102f5b9b
raimeiga <yusuke612@hotmail.com>
2024-01-18 12:19:06 +0900

    9.3 ヘッダーにロゴを追加

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light shadow-sm samuraimart-header-container">
    <div class="container">
-     <a class="navbar-brand" href="{{ url('/') }}">
-       {{ config('app.name', 'Laravel') }}
+     <a class="navbar-brand" href="{{ url('/') }}"> 
+       <img src="{{asset('img/logo.jpg')}}">
      </a>
      <form class="row g-1">
        <div class="col-auto">
