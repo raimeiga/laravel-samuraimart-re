8c4799b4ab2c954d5bc3f8bc91f68b62f2e67eb0
raimeiga <yusuke612@hotmail.com>
2024-01-19 22:20:24 +0900

    7.1 トップページに新着商品を表示/コントローラーを修正

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\MajorCategory;
+use App\Models\Product;
 
 class WebController extends Controller
 {
@@ -15,7 +16,8 @@ class WebController extends Controller
 
         $major_categories = MajorCategory::all();
 
-        return view('web.index', compact('major_categories', 'categories'));
-    }
+        $recently_products = Product::orderBy('created_at', 'desc')->take(4)->get();
 
+       return view('web.index', compact('major_categories', 'categories', 'recently_products'));
+    }
 }
